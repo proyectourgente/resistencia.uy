49a94d39eb966ec6e1c3f62c32e7791d43e4da66
raúl <rsperoni@magnesium.coop>
2020-12-29 09:59:30 -0300

    footer, icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,10 +29,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#b91c1c`,
+        theme_color: `#b91c1c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/triangulo.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/layout.js
+++ src/components/layout.js
@@ -32,9 +32,27 @@ const Layout = ({ children }) => {
       <div className="container mx-auto px-5 lg:px-20 ">
         <main>{children}</main>
         <footer className="mt-10">
-          {new Date().getFullYear()}, Hecho con
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>. Este proyecto está en construcción y pretende colaborar con el intento de derogar los artículos más dañinos de la LUC.
+            <div className="mx-auto p-5 w-100 lg:w-1/2 font-sans text-center">
+
+                <h3 className="text-red-700 text-sm">¿Encontraste algún error? ¿Tenés sugerencias? ¿Querés colaborar?</h3>
+                <div className="flex w-1/3 lg:w-10 mx-auto mt-5">
+                    <a className="px-2 text-red-700 hover:text-gray-100 visited:text-gray-300" href="https://twitter.com/raulsperoni" target="_blank" title="Twitter" rel="noopener noreferrer">
+                        <FaTwitter />
+                    </a>
+                    <a className="px-2 text-red-700 hover:text-gray-100 visited:text-gray-300" href="https://github.com/raulsperoni/resistencia.uy" target="_blank" title="Github" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
+                </div>
+
+                <span className=" text-xs text-red-700">
+                <p className="mt-5">
+                    {new Date().getFullYear()}, Hecho con
+                    {` `}
+                    <a href="https://www.gatsbyjs.com">Gatsby</a>. Este proyecto está en construcción y pretende colaborar con el intento de derogar los artículos más dañinos de la LUC.
+
+                </p>
+                    </span>
+            </div>
 
         </footer>
       </div>
